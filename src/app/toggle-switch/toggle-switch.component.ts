d7a22eb4e754dbba33c684ea24acc3db8c4f8fca
Mira <mira.schaar@gmx.net>
2023-11-11 00:04:09 +0100

    added toggle switch component

---
+++ src/app/toggle-switch/toggle-switch.component.ts
@@ -0,0 +1,22 @@
+import {Component, EventEmitter, Output} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FormsModule} from "@angular/forms";
+
+@Component({
+  selector: 'app-toggle-switch',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './toggle-switch.component.html',
+  styleUrls: ['./toggle-switch.component.css'],
+})
+export class ToggleSwitchComponent {
+  @Output() toggleChange = new EventEmitter<boolean>();
+  isToggled: boolean = false;
+
+  // Make sure this method exists
+  onToggleChange() {
+    this.isToggled = !this.isToggled;
+    this.toggleChange.emit(this.isToggled);
+  }
+
+}
