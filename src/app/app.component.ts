d1d28a21ff4d385a523097b13a923001728df42a
Mira <mira.schaar@gmx.net>
2023-11-11 00:03:47 +0100

    created a simple design for the textanalyzer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import {Component, ViewEncapsulation} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import {FormsModule} from "@angular/forms";
+import {ToggleSwitchComponent} from "./toggle-switch/toggle-switch.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, FormsModule],
+  imports: [CommonModule, RouterOutlet, FormsModule, ToggleSwitchComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 
@@ -16,9 +17,18 @@ import {FormsModule} from "@angular/forms";
 export class AppComponent {
   text: string = '';
   result: string | null = null;
+  analysisType: string = 'both'; // Default selection
 
 
   analyzeText(): void {
     this.result = 'Ergebnis der Analyse: ' + this.text;
   }
+
+  // Inside app.component.ts
+  handleToggle(toggled: boolean) {
+    // toggled will be a boolean
+    console.log('Toggle is now: ', toggled);
+  }
+
+
 }
